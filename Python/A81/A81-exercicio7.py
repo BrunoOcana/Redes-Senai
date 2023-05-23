c96362b0ca05cbdea97b6a5b113558485bc01a6e
Bruno Ocana <brunoocana@gmail.com>
2023-05-23 20:02:32 -0300

    A81 - Exercícios extras - Update 1

--- Python/A81/A81-exercicio7.py
+++ Python/A81/A81-exercicio7.py
@@ -1,3 +1,12 @@
 # Crie um programa para ler a temperatura em Celsius e apresentar a
-# temperatura convertida em Farenheit.
-# A fórmula para conversão: F=(9*C+160)/5
+# temperatura convertida em Farenheit. A fórmula para conversão: F=(9*C+160)/5
+
+print ("""Olá usuário.
+Você quer verificar qual é a conversão da temperatura para retarded units?""")
+cels = float(input("""Informe, por favor, qual é a temperatura em Celsius:
+Temperatura: """))
+faren = round((9 * cels + 160) / 5, 2)
+temp = {"Celsius" : cels, "Farenheit" : faren}
+
+#Resultado
+print (f"A temperatura {cels}ºC em Farenheit é de {faren}ºF.")
--- Python/A81/A81-exercicio8.py
+++ Python/A81/A81-exercicio8.py
@@ -1,3 +1,16 @@
 # Desenvolva um script em Python que receba um valor em reais (R$) e converta-o
 # para dólares. Considere que $1 = R$4,98. No resultado, exiba apenas duas casas
 # decimais após a vírgula.
+
+print ("""Olá usuário.
+Você quer verificar qual a conversão de um valor em reais para dólar?""")
+brl = float(input("""Informe, por favor, qual é o valor em reais.
+R$: """))
+
+def converter():
+    usd = round(brl / 4.98, 2)
+    moedas = {"Real" : brl, "Dolar" : usd}
+    return moedas
+
+conversao = converter()
+print("USD: {}".format(conversao["Dolar"]))
