e93b0dcf159195960cc647c2c7ba1496b7dc9a55
Bruno Ocana <brunoocana@gmail.com>
2023-05-24 09:11:53 -0300

    A82 - Antes do intervalo.

--- Python/A82/A82-desafio1.py
+++ Python/A82/A82-desafio1.py
@@ -1,2 +1,12 @@
 # Desenvolva um programa que leia a ideade de uma pessoa e informe se ela é
 # maior ou menor que 18 anos.
+
+user = input("Por favor, informe seu nome.")
+print(f"Olá {user}, por favor verifique sua idade.")
+
+idade = input("Idade: ")
+
+if idade >= 18:
+    print ("Você pode acessar o site normalmente.")
+elif idade < 18:
+    print ("Você não pode acessar o conteúdo desse site.")
