51eebe96fbfbdf93125eb99947a880a1de72f10f
Bruno Ocana <brunoocana@gmail.com>
2023-05-25 09:36:49 -0300

    A83 - Off.

--- Python/A83/A83-repeticoes.py
+++ Python/A83/A83-repeticoes.py
@@ -1,11 +1,12 @@
 # Aprender formas de repetir um código
 
+# Pode ser usado a função for + in range para criar loops
 print("Você quer saber quanto teria, a cada dia, se recebesse R$237/dia?")
 escolha = str(input("Sua resposta (Sim ou Não): "))
 
+# Avaliação da escolha
 if (escolha.lower()) == "sim":
     for m in range(1,31):
         print (f"R$",237*m)
 else:
     print("Ok...")
-#if escolha == "Sim" or escolha == "sim":
