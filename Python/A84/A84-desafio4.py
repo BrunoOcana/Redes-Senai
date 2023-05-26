9a23e5b0fbd8c98388a7d0ec251f72cfa0c1113b
Bruno Ocana <brunoocana@gmail.com>
2023-05-26 10:51:40 -0300

    .

--- Python/A84/A84-desafio4.py
+++ Python/A84/A84-desafio4.py
@@ -2,3 +2,13 @@
 # argumento e verifica se o número é par ou não. A função deve retornar True se o
 # número for par e False caso contrário. Em seguida, chame a função com diferentes
 # valores e imprima o resultado.
+
+def verif_par(n1):
+    nfinal = n1 / 2
+    if nfinal / 2 == type(int):
+        return True
+    else:
+        return False
+    
+print("\nVerifique se o número é par.")
+numero = int(input("Escolha: "))
